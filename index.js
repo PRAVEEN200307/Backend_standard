24dcff68bba45dbd186f4023fd51574923ae33d6
praveen <crouselearn@gmail.com>
2025-07-20 17:12:02 +0530

    initail

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express, { Router } from "express"
 import router from "./Routes/routes.js"
+import { ConnectDb } from "./Db/server.js";
 const app = express();
 const port = 3000
-app.use(router)
+app.use(router);
+
+ConnectDb();
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })

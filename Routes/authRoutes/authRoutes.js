96a58176713019c22a5ef366a9099aa98a485bf5
praveen <crouselearn@gmail.com>
2025-07-20 23:20:58 +0530

    Update authRoutes.js

--- Routes/authRoutes/authRoutes.js
+++ Routes/authRoutes/authRoutes.js
@@ -1,16 +1,13 @@
 import express from "express";
-import { createNewAdmin, getAllAdmin, updateAdmin,deleteAdmin } from "../../Controllers/authController.js";
+import { createNewAdmin, getAllAdmin, updateAdmin, deleteAdmin } from "../../Controllers/authController.js";
 import { upload } from "../../middleware/uploadMiddleware.js";
 
 const router = express.Router();
 
 router.get('/', getAllAdmin);
 router.get('/:id', getAllAdmin);
-
-router.post('/',upload.fields([{name:'image', maxCount:1},{name:'document',maxCount:1}]) ,createNewAdmin);
-
+router.post('/', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'document', maxCount: 1 }]), createNewAdmin);
 router.put('/:id', updateAdmin)
-
 router.delete('/:id', deleteAdmin)
 
 export default router

e75d297405c57f20e25229cbd015d4969c2bd79b
praveen <crouselearn@gmail.com>
2025-07-20 17:45:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import { ConnectDb } from "./Db/server.js";
 const app = express();
 const port = 3000
 app.use(router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 
 ConnectDb();
 app.listen(port, () => {
